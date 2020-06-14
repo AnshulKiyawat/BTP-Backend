ee5ecc9f119dfea2c5f05d96828d3dbfac172549
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-06-14 17:22:52 +0530

    Add files via upload

--- api/login.py
+++ api/login.py
@@ -1,26 +1,77 @@
 from django.http import JsonResponse
 import json
 
+from database_models.models import Users
+
+
 def signUp(request):
     if(request.method=='POST'):
         input_json = json.loads(request.body)
 
         email = input_json['email']
         first_name = input_json['firstName']
+        middle_name = input_json['middleName']
         last_name = input_json['lastName']
         password = input_json['password']
-        print(email)
-        print(first_name)
-        print(last_name)
-        print(password)
-        '''
-        TODO:
-        -----
-        Check if email already present in database.
-        If not, add email, pwd, firstname,lastname to database and return "SUCCESS"
-        If yes, return message "FAILURE"
-        '''
+
+        try:
+            Users.objects.get(email=email)
+            returnJson={
+                'message': "FAILURE",
+                'reason' : "User Already Exists"
+            }
+        except:
+            if not email.endswith('@lnmiit.ac.in'):    
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Email does not end with '@lnmiit.ac.in'"
+                }
+            elif len(first_name) == 0:
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "First name is necessary"
+                }
+            elif len(password) < 8:
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password less than 8 characters"
+                }
+            elif len(password) > 30:
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password longer than 30 characters"
+                }
+            elif not any(char.isdigit() for char in password):
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password should have atleast one digit from 0-9"
+                }
+            elif not any(char.isupper() for char in password):
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password should have atleast one uppercase ASCII character"
+                }
+            elif not any(char.islower() for char in password):
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password should have atleast one lowercase ASCII character"
+                }
+            elif not any(char in ['!','@','#','$','%','^','&','*','(',')'] for char in password):
+                returnJson={
+                    'message': "FAILURE",
+                    'reason': "Password should have atleast special symbol among: !,@,#,,$,%,^,&,*,(,)"
+                }
+            else:
+                 Users(email=email,password=password,first_name=first_name,last_name=last_name).save()
+                 returnJson={
+                    'message': "SUCCESS",
+                    'reason': "User created"
+                }
+
+    else:
         returnJson={
-            'message': "SUCCESS"
+            'message': "FAILURE",
+            'reason': "Server could not process the data sent."
         }
-        return JsonResponse(returnJson)
+        
+    return JsonResponse(returnJson)
